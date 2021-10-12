7d38bed6493b7aa76771c70e56c49399f0a168a4
ScorpioneOrzion <36512390+ScorpioneOrzion@users.noreply.github.com>
2021-10-12 19:05:16 +0200

    added small constrain

--- data/galaxyGeneration.js
+++ data/galaxyGeneration.js
@@ -2,16 +2,23 @@
 export default function generateLocations(playerCount) {
   let layer = 1;
   let current = 0;
-  const locations = [];
+  let locations = [];
+  let layers = [];
 
   for (let i = 0; i < playerCount; i++) {
     let max = Math.pow(layer, 2) * 2;
     locations.push({ layer, current })
+    layers.push(layer)
     current += 1;
     if (current >= max) {
       layer += 1;
       current = 0;
     }
   }
+
+  if (locations.filter(value => value.layer === Math.max(...layers).length) <= 2) {
+    locations = locations.map(value => { return value.layer === Math.max(...layers) ? { current: value.current + 2 , layer: 1 } : value })
+  }
+
   return locations.map((a, _, b) => { return { angle: Math.PI * 2 * a.current / b.filter(c => c.layer === a.layer).length, distance: a.layer } });
 }
